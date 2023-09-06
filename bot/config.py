de4b225f8e12f9342e0bca93590e59f1924a886d
Sobirov Jamshid <75526925+Sobirov-Jamshid@users.noreply.github.com>
2023-09-06 09:29:13 +0500

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -19,4 +19,4 @@ sys.path.append(APP_DIR_PATH)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 django.setup()
 
-TOKEN = '6278927189:AAH0Gv24uxdq0zeRCoEADR2uuHxYXOwMKb8'
+TOKEN = '6278927189:AAH0Gv24uxdq0zeRCoEADR2uuH'
